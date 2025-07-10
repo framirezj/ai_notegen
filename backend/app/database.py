10ed6d25da159a3d61c029dfced5aad2781355d0
framirezj <12649259+framirezj@users.noreply.github.com>
2025-07-10 13:55:12 -0400

    chore: dockerización mínima con persistencia de base de datos
    - Se dockeriza backend y frontend para producción
    - Se mueve note.db a carpeta persistente /app/data
    - Se configura volumen db_data en docker-compose
    - Se ajusta conexión a SQLite para nueva ruta

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-#constante de la donde esta ubicada la base de datos
-SQLALCHEMY_DATABASE_URL = "sqlite:///./notes.db"
+# URL de la base de datos, apuntando a una carpeta de datos dedicada y persistente
+SQLALCHEMY_DATABASE_URL = "sqlite:///app/data/notes.db"
 
 #Crea el motor de la base de datos usando la URL anterior. 
 #El argumento check_same_thread=False permite que la base de datos SQLite sea usada por varios hilos (útil en aplicaciones web).
@@ -13,5 +13,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 #Crea una clase base para los modelos ORM. Todos los modelos de la base de datos deben heredar de esta clase.
 base = declarative_base()
-
 
