cfd51052e4bc28811a9e076618c88c3bd3a1ef03
framirezj <12649259+framirezj@users.noreply.github.com>
2025-07-11 13:18:11 -0400

    feat: se crea endpoint para eliminar notas de la base de datos

--- backend/app/routes/notes.py
+++ backend/app/routes/notes.py
@@ -37,4 +37,11 @@ def get_notes(db: Session = Depends(get_db)):
     #ocupamos el metodo query de la Session a la bd y traemos todos los registros de la tabla Note
     return db.query(models.Note).all()
 
-    
+
+@router.delete("/notes/{note_id}", status_code=204)
+def delete_note(note_id: int ,db: Session = Depends(get_db)):
+    db_note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if not db_note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    db.delete(db_note)
+    db.commit()
