7757d5b4ffe32f96991151aa109f96a212faab0a
framirezj <12649259+framirezj@users.noreply.github.com>
2025-07-14 15:02:04 -0400

    "chore: migración de backend a PostgreSQL
    - Se eliminó el uso de SQLite
    - Se configuró conexión a PostgreSQL usando .env
    - Se actualizó database.py para levantar la conexión desde DATABASE_URL
    - Se probó exitosamente conexión con base de datos externa en red local"

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,16 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# URL de la base de datos, apuntando a una carpeta de datos dedicada y persistente
-SQLALCHEMY_DATABASE_URL = "sqlite:///app/data/notes.db"
+load_dotenv()
 
-#Crea el motor de la base de datos usando la URL anterior. 
-#El argumento check_same_thread=False permite que la base de datos SQLite sea usada por varios hilos (útil en aplicaciones web).
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-#crea una sesión para interactuar con la base de datos
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definida en el archivo .env")
+
+
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#Crea una clase base para los modelos ORM. Todos los modelos de la base de datos deben heredar de esta clase.
 base = declarative_base()
+
+print("✅ Conectado a la base de datos PostgreSQL")
 
